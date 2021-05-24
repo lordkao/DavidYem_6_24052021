2b9588d6d964872aed7b8414765a598e3f0ee881
David Yem <david13200arles@hotmail.fr>
2021-05-24 16:59:58 +0200

    login user

--- controllers/user.js
+++ controllers/user.js
@@ -13,6 +13,23 @@ exports.signup = (req,res,next) => {/*Création d'un nouvel utilisateur.*/
     })
     .catch( error => res.status(500).json({ error }))
 }
-exports.login = (req,res,next) => {
-    
+exports.login = (req,res,next) => {/*Connexion utilisateur.*/
+    User.findOne({ email: req.body.email})
+    .then( user => {
+        if(!user){
+            return res.status(400).json({ error:'Utilisateur non trouvé !' })
+        }
+        bcrypt.compare(req.body.password,user.password)
+        .then( valid => {
+            if(!valid){
+                return res.status(401).json({ error : 'mot de passe incorrect !'})
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN'
+            })
+        })
+        .catch( error => res.status(500).json({ error }))
+    })
+    .catch( error => res.status(500).json({ error }))
 }
