8de81d5c62615670f5be2088aee839df231d9a17
David Yem <david13200arles@hotmail.fr>
2021-05-24 15:21:18 +0200

    routes et controllers mis en place

---
+++ routes/stuff.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const stuffCtrl = require('../controllers/stuff')
+
+
+router.post('/',stuffCtrl.createThing)
+
+router.get('/:id',stuffCtrl.getOneThing)
+
+router.put('/:id',stuffCtrl.modifyThing)
+
+router.delete('/:id',stuffCtrl.deleteThing)
+
+router.get('/',stuffCtrl.getAllThings)
+
+module.exports = router
