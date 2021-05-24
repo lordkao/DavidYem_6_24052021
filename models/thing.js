7df4b779d2de5c8ef4f707f7cc525a14092acf18
David Yem <david13200arles@hotmail.fr>
2021-05-24 22:29:31 +0200

    multer

--- models/thing.js
+++ models/thing.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
 const sauceSchema = mongoose.Schema({
-    name : { type: String, required : true},/*Nom de la sauce.*/
-    manufacturer : { type: String, required: true},/*Fabricant de la sauce.*/
-    description : { type: String, required: true},/*Description de la sauce.*/
-    mainPepper : { type: String, required: true},/*Principal ingrédient dans la sauce.*/
-    imageUrl : { type: String, required: true},/*string de l'image de la sauce téléchargée.*/
-    heat : { type: Number, required: true},/*Nombre entre 1 et 10 décrivant la sauce.*/
-    likes : { type: Number, required: true},/*Nombre de users qui aiment la sauce.*/
-    dislikes : { type: Number, required: true},/*Nombre de users qui n'aiment pas la sauce.*/
-    usersLiked: [{type: String, required: true}] ,/*Tableau d'IDs des users qui ont aimé la sauce.*/
-    usersDisliked: [{type: String, required: true}] /*Tableau d'IDs des users qui n'ont pas aimé la sauce.*/
+    name : { type: String, required : true},        /*Nom de la sauce.*/
+    manufacturer : { type: String, required: true},     /*Fabricant de la sauce.*/
+    description : { type: String, required: true},      /*Description de la sauce.*/
+    mainPepper : { type: String, required: true},       /*Principal ingrédient dans la sauce.*/
+    imageUrl : { type: String, required: true},     /*string de l'image de la sauce téléchargée.*/
+    heat : { type: Number, required: true},     /*Nombre entre 1 et 10 décrivant la sauce.*/
+    likes : { type: Number, required: true},        /*Nombre de users qui aiment la sauce.*/
+    dislikes : { type: Number, required: true},     /*Nombre de users qui n'aiment pas la sauce.*/
+    usersLiked: [{type: String, required: true}],       /*Tableau d'IDs des users qui ont aimé la sauce.*/
+    usersDisliked: [{type: String, required: true}]     /*Tableau d'IDs des users qui n'ont pas aimé la sauce.*/
 })
 
 module.exports = mongoose.model('Thing',sauceSchema)
