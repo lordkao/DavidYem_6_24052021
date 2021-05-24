e9b0f8205453970cea8c21e3d9a1d374fc431af6
David Yem <david13200arles@hotmail.fr>
2021-05-24 14:41:47 +0200

    obtenir une sauce par son id

--- app.js
+++ app.js
@@ -23,12 +23,18 @@ app.use(bodyParser())/*Analyse le corps de la requête.*/
 
 app.post('/api/sauces',(req,res,next) => {/*Création de sauce.*/
     delete req.body._id
-    const thing = new Thing({
+    const thing = new Thing({/*Créer une nouvelle instance de notre modèle Thing en enregistrant les élements du corps de la requête.*/
         ...req.body
     })
-    thing.save()
-    .then( () => res.status(200).json({ message: 'Objet enregistré !'}))
+    thing.save()/*Enregistre le notre objet dans la base de données.*/
+    .then( () => res.status(201).json({ message: 'Objet enregistré !'}))
     .catch( error => res.status(400).json({ error }))
+})
+
+app.get('/api/sauces/:id',(req,res,next) => { /*Obtenir une sauce par son id.*/
+    Thing.findOne({ _id: req.params.id})
+    .then( thing => res.status(200).json(thing))
+    .catch( error => res.status(404).json({ error }))
 })
 
 app.get('/api/sauces',(req,res,next) => {/*Obtenir toutes les sauces.*/
