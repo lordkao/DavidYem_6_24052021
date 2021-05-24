4ec9816968c1dc857d656de6c5c9dbe4bb84a818
David Yem <david13200arles@hotmail.fr>
2021-05-24 13:51:34 +0200

    mongoDB connexion

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.use((req,res) => {
-    res.json({
-        message: 'Nouvelle réponse du serveur David !!'
+/*Connection de l'api avec mongoDB.*/
+mongoose.connect('mongodb+srv://Lordkao:DatabasemongoDB@serveur-test.zd6sr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req,res,next) => {/*Paramétrage du CORS laissant l'accès à tous les types requêtes.*/
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-requested-with, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS')
+    next()
+})
+app.use(bodyParser())/*Analyse le corps de la requête.*/
+
+app.post('/api/sauces',(req,res,next) => {/*Création de sauce.*/
+    console.log(req.body)
+    res.status(201).json({
+        message :' Sauce créée !'
     })
 })
+
+app.get('/api/sauces',(req,res,next) => {/*Obtenir toutes les sauces.*/
+    console.log('Envoi de la liste de toutes les sauces')
+    res.status(201).json('Envoi de toutes les sauces !!')
+})
+
 
 module.exports = app
