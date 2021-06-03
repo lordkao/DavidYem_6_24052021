7694c83b2197ac08ddb8ea3f74a991f327a0fd0f
David Yem <david13200arles@hotmail.fr>
2021-06-03 12:19:52 +0200

    validation entrées login

--- controllers/user.js
+++ controllers/user.js
@@ -6,6 +6,8 @@ const tokenLogin = process.env.TOKEN_LOGIN
 
 
 exports.signup = (req,res,next) => {/*Création d'un nouvel utilisateur.*/
+
+
     bcrypt.hash(req.body.password,10)
     .then( hash => {
         const user = new User({
@@ -19,7 +21,39 @@ exports.signup = (req,res,next) => {/*Création d'un nouvel utilisateur.*/
     .catch( error => res.status(500).json({ error }))
 }
 exports.login = (req,res,next) => {/*Connexion utilisateur.*/
-    User.findOne({ email: req.body.email})
+    if(!(/^([\w.-]+)[@]{1}([\w]+)[.]{1}([a-z]){2,5}$/.test(req.body.email))){
+        res.status(400).json({ message:'Veuillez renseigner un email valide.(ex: jean@hotmail.com'})
+        }
+    else if(/([^a-zA-Z0-9@]+)/.test(req.body.password)){
+        res.status(400).json({ message:'Veuillez renseigner un password valide.(les caractères spéciaux ne sont pas autorisés)'})
+    }    
+    else{
+        User.findOne({ email: req.body.email})
+        .then( user => {
+            if(!user){
+                return res.status(400).json({ error:'Utilisateur non trouvé !' })
+            }
+            bcrypt.compare(req.body.password,user.password)
+            .then( valid => {
+                if(!valid){
+                    return res.status(401).json({ error : 'mot de passe incorrect !'})
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        { userId: user._id},
+                        tokenLogin,
+                        { expiresIn: '24h'}
+                    )
+                })
+            })
+            .catch( error => res.status(500).json({ error }))
+        })
+        .catch( error => res.status(500).json({ error }))
+    }
+}        
+
+/*User.findOne({ email: req.body.email})
     .then( user => {
         if(!user){
             return res.status(400).json({ error:'Utilisateur non trouvé !' })
@@ -40,5 +74,4 @@ exports.login = (req,res,next) => {/*Connexion utilisateur.*/
         })
         .catch( error => res.status(500).json({ error }))
     })
-    .catch( error => res.status(500).json({ error }))
-}
+    .catch( error => res.status(500).json({ error }))*/
